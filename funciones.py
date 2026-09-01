c531a03ca2e3707c77523681093c474931a38ce0
gracecss18 <higraciela@hotmail.com>
2026-09-01 11:19:10 -0500

    comence la parte intermedia de los ejercicios

--- funciones.py
+++ funciones.py
@@ -21,10 +21,36 @@ print("--------------------------------------------")
 # Ejercicio 2: Crea una función "es_mayor_edad" que reciba una edad 
 # y devuelva (return) True si es mayor o igual a 18, o False si no
 
-def esMayorDeEdad (edad):
+def esMayorDeEdad(edad):
     if edad >= 18:
         return True
-    else:
+    else: 
         return False
 
-print(f"Usted, {esMayorDeEdad} ")
+print(esMayorDeEdad(20))
+print(esMayorDeEdad(15))
+print(esMayorDeEdad(90))
+print(esMayorDeEdad(14))
+print("--------------------------------------------")
+print("--------------------------------------------")
+
+# Ejercicio 3: Crea una función "calcular_precio_con_iva" que 
+# reciba un precio y devuelva ese precio con el 19% de IVA agregado
+
+def calcularPrecioIva (precio):
+    iva = precio * 0.19
+    precioFinal = precio + iva
+    return precioFinal
+
+print(f"Su precio final es: {calcularPrecioIva(30)}")
+print("--------------------------------------------")
+print("--------------------------------------------")
+
+
+# --------------------------------------------
+# NIVEL INTERMEDIO
+# --------------------------------------------
+
+# Ejercicio 4: Crea una función "contar_vocales" que reciba una 
+# palabra y devuelva cuántas vocales tiene
+
